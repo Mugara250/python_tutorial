f6075af8984f03062157a1facddc6706e0b779ac
Mugara250 <mbonyumugara52@gmail.com>
2025-11-07 23:40:21 +0200

    strings

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print("Hello world!")
+
+course_name = "Python Programming language"
+print(len(course_name))
+print(course_name[4])
+print(course_name[0:10])
+print(course_name[0:])
+print(course_name[:20])
+print(course_name[:])
