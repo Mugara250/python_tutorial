3ac2aa089fcad77b480bb6fb0b5daa6562df3a35
Mugara250 <mbonyumugara52@gmail.com>
2025-11-08 00:02:01 +0200

    strings

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ print(course_name[0:10])
 print(course_name[0:])
 print(course_name[:20])
 print(course_name[:])
+
+first = "  John  "
+last = "Doe"
+residence = "banana"
+
+print(f"{first} {last}")
+print(first.upper())
+print(last.lower())
+print(residence.capitalize())
+print(first.strip())
+print(course_name.find("lang"))
+print(residence.replace('b', 'B'))
+print('guage' in course_name)
+print('nun' not in residence)
