9ede5e402d5176b8fc42369796062562d4ee0bb9
Mugara250 <mbonyumugara52@gmail.com>
2025-11-07 23:42:19 +0200

    escape sequence

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 print("Hello world!")
 
-course_name = "Python Programming language"
+course_name = "\"Python\" Programming language"
 print(len(course_name))
 print(course_name[4])
 print(course_name[0:10])
